caf5acce4608559b78941d47589f18e282eb8e2e
clecardona <clecardona@gmail.com>
2021-09-26 19:34:12 +0200

    rename reload,setreload

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,16 @@ import ParcelsContainer from "./components/ParcelsContainer";
 export default function App() {
   const [t, i18n] = useTranslation("common");
   const languageLocal = JSON.parse(localStorage.getItem("lang"));
-  const [state, setstate] = useState(true);
+  const [reload, setReload] = useState(true);
 
   function selectLanguage(value) {
     localStorage.setItem("lang", JSON.stringify(value));
-    setstate(!state);
+    setReload(!reload);
   }
 
   useEffect(() => {
     i18n.changeLanguage(languageLocal);
-  }, [state, i18n, languageLocal]);
+  }, [reload, i18n, languageLocal]);
 
   return (
     <div className="App">
